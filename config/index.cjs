13cf707892c8346162b160b21ebc2a437f7a2ae6
YANAAADS <139674661+YANAAADS@users.noreply.github.com>
2023-07-16 16:24:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,10 +23,14 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oUVbQ6rbh5M6y9dygj3dp_T6XbMs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '		i0nItaubbewe8HTiIBvIUDt6Oi-a01O19rBG-IE-HD8',
+      useTemplateId: '	-TPH2zkqu4VxI5A86ASSmdskJwy_WfMSpW7QsrvN5uM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-07',
       festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '张怡静', year: '2004', date: '09-07',
+        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '张怡静', year: '2004', date: '07-23',
@@ -35,19 +39,19 @@ const USER_CONFIG = {
           type: '节日', name: '相识纪念日', year: '2022', date: '09-03',
         },
       ],
-      // 我们遇见已经有xxxx天了的配置
+      // 我们认识已经有xxxx天了的配置
       customizedDateList: [
-        // 遇见的日子
+        // 相识的日子
         { keyword: 'love_day', date: '2022-09-03' },
-        // 相识纪念日
-        { keyword: 'marry_day', date: '2023-07-23' },
+        // 遇见纪念日
+        { keyword: 'marry_day', date: '2023-07-11' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '		i0nItaubbewe8HTiIBvIUDt6Oi-a01O19rBG-IE-HD8',
+  CALLBACK_TEMPLATE_ID: '	-TPH2zkqu4VxI5A86ASSmdskJwy_WfMSpW7QsrvN5uM',
 
   CALLBACK_USERS: [
     {
