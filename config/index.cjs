a664e9fd804f3aa76cc1269280961d2692c32553
YANAAADS <139674661+YANAAADS@users.noreply.github.com>
2023-07-16 16:37:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '张怡静',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oUVbQ6rbh5M6y9dygj3dp_T6XbMs，oUVbQ6mDd_4Ycf7PITBEDnjZpgmQ',
+      id: 'oUVbQ6mDd_4Ycf7PITBEDnjZpgmQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '-TPH2zkqu4VxI5A86ASSmdskJwy_WfMSpW7QsrvN5uM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
