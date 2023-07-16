320251e724e7bb47337b8dc4842bb0563dde859f
YANAAADS <139674661+YANAAADS@users.noreply.github.com>
2023-07-16 16:38:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oUVbQ6rbh5M6y9dygj3dp_T6XbMs',
+      id: 'oUVbQ6mDd_4Ycf7PITBEDnjZpgmQ',
     }
   ],
 
